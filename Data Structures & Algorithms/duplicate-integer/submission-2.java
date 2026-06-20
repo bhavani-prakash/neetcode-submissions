3c02476ae52ce15ee205a10f2d8793d82cb6cf4b
P. BHAVANI PRAKASH <23691a0424@mits.ac.in>
2026-06-20 11:01:42 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,29 @@
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        // boolean isdupli=false;
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=0;j<nums.length;j++){
+        //         if(i==j){
+        //             continue;
+        //         }
+        //         if(nums[i]==nums[j]){
+        //             isdupli=true;
+        //             return isdupli;
+        //         }
+        //     }
+           
+        // }
+        // return isdupli;
+
+        Hashtable<Integer,Integer> ht = new Hashtable<>();
+        for(int num:nums){
+            if(ht.containsKey(num)){
+                return true;
+            }
+            ht.put(num,1);
+        }
+        return false;
+        
+    }
+}
